bc68cf4316da2b4c7e92e9267c8b42c5ed9f91c5
Kevin Barnes <kbarnes3@gmail.com>
2023-12-03 12:02:01 -0800

    Starting Day 03 Part 1

---
+++ day03_gear_ratios_part1/src/main.rs
@@ -0,0 +1,11 @@
+#![allow(unused_imports)]
+use day03_gear_ratios_common::SAMPLE_DATA;
+
+fn main() {
+    let result = do_work(SAMPLE_DATA);
+    println!("{}", result);
+}
+
+fn do_work(data: u64) -> u64 {
+    data
+}
