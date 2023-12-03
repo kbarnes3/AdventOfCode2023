66a538c929ac94dbe3a385b93ea44196cb508e98
Kevin Barnes <kbarnes3@gmail.com>
2023-12-02 16:06:17 -0800

    Day 1 Part 1 sample works

--- day01_trebuchet_common/src/lib.rs
+++ day01_trebuchet_common/src/lib.rs
@@ -1,1 +1,8 @@
 pub const MESSAGE: &str = "Hello, world!";
+
+pub const SAMPLE_DATA: [&str; 4] = [
+    "1abc2",
+    "pqr3stu8vwx",
+    "a1b2c3d4e5f",
+    "treb7uchet",
+];
--- day01_trebuchet_part1/src/main.rs
+++ day01_trebuchet_part1/src/main.rs
@@ -1,5 +1,23 @@
-use day01_trebuchet_common::MESSAGE;
+use day01_trebuchet_common::SAMPLE_DATA;
 
 fn main() {
-    println!("{}", MESSAGE);
+    let result = do_work(SAMPLE_DATA);
+    println!("{}", result);
+}
+
+fn do_work<const N: usize>(data: [&str; N]) -> u64 {
+    let mut result: u64 = 0;
+    for line in data {
+        result += get_number_from_line(line);
+    }
+
+    result
+}
+
+fn get_number_from_line(line: &str) -> u64 {
+    let first_number: u64 = 
+        line.chars().into_iter().find(|c| c.is_numeric()).unwrap_or('0').to_digit(10).unwrap_or(0) as u64;
+    let last_number: u64 = line.chars().rev().find(|c| c.is_numeric()).unwrap_or('0').to_digit(10).unwrap_or(0) as u64;
+
+    first_number * 10 + last_number
 }
