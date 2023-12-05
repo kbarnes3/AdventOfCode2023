3260c26ff45da31a9a502a62fa401026730a746d
Kevin Barnes <kbarnes3@gmail.com>
2023-12-04 17:18:37 -0800

    Day 04 Part 1 sample data works

--- day04_scratchcards_part1/src/main.rs
+++ day04_scratchcards_part1/src/main.rs
@@ -1,11 +1,25 @@
-#![allow(unused_imports)]
-use day04_scratchcards_common::SAMPLE_DATA;
+use std::collections::HashSet;
+#[allow(unused_imports)]
+use day04_scratchcards_common::{ScratchCard, SAMPLE_DATA};
 
 fn main() {
     let result = do_work(SAMPLE_DATA);
     println!("{}", result);
 }
 
-fn do_work(data: u64) -> u64 {
-    data
+fn do_work<const W: usize, const N: usize, const C: usize>(data: [ScratchCard<W, N>; C]) -> u64 {
+    data.iter().map(get_point_value).sum()
+}
+
+fn get_point_value<const W: usize, const N: usize>(card: &ScratchCard<W, N>) -> u64 {
+    let winning_numbers = HashSet::from(card.winning_numbers);
+    let numbers = HashSet::from(card.numbers);
+
+    let matching_numbers = winning_numbers.intersection(&numbers).count();
+
+    if matching_numbers == 0 {
+        0
+    } else {
+        2u64.pow((matching_numbers as u32) - 1)
+    }
 }
