e292e29a3cf3e73be6ca84df82d2a47442ea8989
Kevin Barnes <kbarnes3@gmail.com>
2023-12-05 18:47:14 -0800

    Day 05 Part 1 sample data works

--- day05_if_you_give_a_seed_a_fertilizer_common/src/lib.rs
+++ day05_if_you_give_a_seed_a_fertilizer_common/src/lib.rs
@@ -1,1 +1,169 @@
-pub const SAMPLE_DATA: u64 = 35;
+pub struct Mapping {
+    pub destination_range_start: u64,
+    pub source_range_start: u64,
+    pub range_length: u64,
+}
+
+pub struct Almanac<
+    const N: usize,
+    const S: usize,
+    const F: usize,
+    const W: usize,
+    const L: usize,
+    const T: usize,
+    const H: usize,
+    const M: usize,
+> {
+    pub seeds: [u64; N],
+    pub seed_to_soil: [Mapping; S],
+    pub soil_to_fertilizer: [Mapping; F],
+    pub fertilizer_to_water: [Mapping; W],
+    pub water_to_light: [Mapping; L],
+    pub light_to_temperature: [Mapping; T],
+    pub temperature_to_humidity: [Mapping; H],
+    pub humidity_to_location: [Mapping; M],
+}
+
+pub const SAMPLE_DATA: Almanac<4, 2, 3, 4, 2, 3, 2, 2> = Almanac {
+    seeds: [79, 14, 55, 13],
+    seed_to_soil: [
+        {
+            Mapping {
+                destination_range_start: 50,
+                source_range_start: 98,
+                range_length: 2,
+            }
+        },
+        {
+            Mapping {
+                destination_range_start: 52,
+                source_range_start: 50,
+                range_length: 48,
+            }
+        },
+    ],
+    soil_to_fertilizer: [
+        {
+            Mapping {
+                destination_range_start: 0,
+                source_range_start: 15,
+                range_length: 37,
+            }
+        },
+        {
+            Mapping {
+                destination_range_start: 37,
+                source_range_start: 52,
+                range_length: 2,
+            }
+        },
+        {
+            Mapping {
+                destination_range_start: 39,
+                source_range_start: 0,
+                range_length: 15,
+            }
+        },
+    ],
+    fertilizer_to_water: [
+        {
+            Mapping {
+                destination_range_start: 49,
+                source_range_start: 53,
+                range_length: 8,
+            }
+        },
+        {
+            Mapping {
+                destination_range_start: 0,
+                source_range_start: 11,
+                range_length: 42,
+            }
+        },
+        {
+            Mapping {
+                destination_range_start: 42,
+                source_range_start: 0,
+                range_length: 7,
+            }
+        },
+        {
+            Mapping {
+                destination_range_start: 57,
+                source_range_start: 7,
+                range_length: 4,
+            }
+        },
+    ],
+    water_to_light: [
+        {
+            Mapping {
+                destination_range_start: 88,
+                source_range_start: 18,
+                range_length: 7,
+            }
+        },
+        {
+            Mapping {
+                destination_range_start: 18,
+                source_range_start: 25,
+                range_length: 70,
+            }
+        },
+    ],
+    light_to_temperature: [
+        {
+            Mapping {
+                destination_range_start: 45,
+                source_range_start: 77,
+                range_length: 23,
+            }
+        },
+        {
+            Mapping {
+                destination_range_start: 81,
+                source_range_start: 45,
+                range_length: 19,
+            }
+        },
+        {
+            Mapping {
+                destination_range_start: 68,
+                source_range_start: 64,
+                range_length: 13,
+            }
+        },
+    ],
+    temperature_to_humidity: [
+        {
+            Mapping {
+                destination_range_start: 0,
+                source_range_start: 69,
+                range_length: 1,
+            }
+        },
+        {
+            Mapping {
+                destination_range_start: 1,
+                source_range_start: 0,
+                range_length: 69,
+            }
+        },
+    ],
+    humidity_to_location: [
+        {
+            Mapping {
+                destination_range_start: 60,
+                source_range_start: 56,
+                range_length: 37,
+            }
+        },
+        {
+            Mapping {
+                destination_range_start: 56,
+                source_range_start: 93,
+                range_length: 4,
+            }
+        },
+    ],
+};
--- day05_if_you_give_a_seed_a_fertilizer_part1/src/main.rs
+++ day05_if_you_give_a_seed_a_fertilizer_part1/src/main.rs
@@ -1,11 +1,90 @@
+use std::collections::HashMap;
+
 #[allow(unused_imports)]
-use day05_if_you_give_a_seed_a_fertilizer_common::SAMPLE_DATA;
+use day05_if_you_give_a_seed_a_fertilizer_common::{Almanac, Mapping, SAMPLE_DATA};
 
 fn main() {
     let result = do_work(SAMPLE_DATA);
     println!("{}", result);
 }
 
-fn do_work(data: u64) -> u64 {
-    data
+fn do_work<
+    const N: usize,
+    const S: usize,
+    const F: usize,
+    const W: usize,
+    const L: usize,
+    const T: usize,
+    const H: usize,
+    const M: usize,
+>(
+    data: Almanac<N, S, F, W, L, T, H, M>,
+) -> u64 {
+    let mappings = build_mapping_ranges(&data);
+    let mut closest_location: Option<u64> = None;
+
+    for seed in data.seeds {
+        let soil = mappings.seed_to_soil.get(&seed).unwrap_or(&seed);
+        let fertilizer = mappings.soil_to_fertilizer.get(soil).unwrap_or(soil);
+        let water = mappings.fertilizer_to_water.get(fertilizer).unwrap_or(fertilizer);
+        let light = mappings.water_to_light.get(water).unwrap_or(water);
+        let temperature = mappings.light_to_temperature.get(light).unwrap_or(light);
+        let humidity = mappings.temperature_to_humidity.get(temperature).unwrap_or(temperature);
+        let location = mappings.humidity_to_location.get(humidity).unwrap_or(humidity);
+
+        match closest_location {
+            None => closest_location = Some(*location),
+            Some(current) => {
+                if *location < current {
+                    closest_location = Some(*location);
+                }
+            }
+        }
+    }
+
+    closest_location.unwrap()
+}
+
+struct MappingRanges {
+    seed_to_soil: HashMap<u64, u64>,
+    soil_to_fertilizer: HashMap<u64, u64>,
+    fertilizer_to_water: HashMap<u64, u64>,
+    water_to_light: HashMap<u64, u64>,
+    light_to_temperature: HashMap<u64, u64>,
+    temperature_to_humidity: HashMap<u64, u64>,
+    humidity_to_location: HashMap<u64, u64>,
+}
+
+fn build_mapping_ranges<
+    const N: usize,
+    const S: usize,
+    const F: usize,
+    const W: usize,
+    const L: usize,
+    const T: usize,
+    const H: usize,
+    const M: usize,
+>(
+    data: &Almanac<N, S, F, W, L, T, H, M>
+) -> MappingRanges {
+    let seed_to_soil = build_mapping_range(&data.seed_to_soil);
+    let soil_to_fertilizer = build_mapping_range(&data.soil_to_fertilizer);
+    let fertilizer_to_water = build_mapping_range(&data.fertilizer_to_water);
+    let water_to_light = build_mapping_range(&data.water_to_light);
+    let light_to_temperature = build_mapping_range(&data.light_to_temperature);
+    let temperature_to_humidity = build_mapping_range(&data.temperature_to_humidity);
+    let humidity_to_location = build_mapping_range(&data.humidity_to_location);
+
+    MappingRanges { seed_to_soil, soil_to_fertilizer, fertilizer_to_water, water_to_light, light_to_temperature, temperature_to_humidity, humidity_to_location }
+}
+
+fn build_mapping_range<const N: usize>(data: &[Mapping; N]) -> HashMap<u64, u64> {
+    let mut result = HashMap::new();
+    for mapping in data {
+        for i in 0..mapping.range_length {
+            result.insert(mapping.source_range_start + i, mapping.destination_range_start + i);
+        }
+    }
+
+    result
 }
