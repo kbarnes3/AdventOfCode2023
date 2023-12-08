fb6d9041be9bb65aaf45c4edd9dfdc16fa6d3ea4
Kevin Barnes <kbarnes3@gmail.com>
2023-12-08 11:36:18 -0800

    Starting Day 02 Part 2

---
+++ day06_wait_for_it_part2/src/main.rs
@@ -0,0 +1,42 @@
+#[allow(unused_imports)]
+use day06_wait_for_it_common::{RaceRecords, REAL_DATA, SAMPLE_DATA};
+
+fn main() {
+    let result = do_work(SAMPLE_DATA);
+    println!("{}", result);
+}
+
+fn do_work<const N: usize>(data: RaceRecords<N>) -> u64 {
+    let mut result = 1;
+
+    for i in 0..N {
+        let time = data.times[i];
+        let distance = data.distances[i];
+        result *= count_winning_options(time, distance);
+    }
+
+    result
+}
+
+fn count_winning_options(time: u64, distance_to_beat: u64) -> u64 {
+    let mut result = 0;
+
+    for hold_time in 0..time {
+        let distance_for_hold_time = get_distance_for_hold_time(time, hold_time);
+        if distance_for_hold_time > distance_to_beat {
+            result += 1;
+        }
+    }
+
+    result
+}
+
+fn get_distance_for_hold_time(total_time: u64, hold_time: u64) -> u64 {
+    if hold_time > total_time {
+        panic!("hold_time must be less than or equal to total_time")
+    }
+
+    let running_time = total_time - hold_time;
+
+    running_time * hold_time
+}
