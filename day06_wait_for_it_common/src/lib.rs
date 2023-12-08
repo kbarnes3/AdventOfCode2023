06129a81f9583235ea5b6b88430f66e65ff43205
Kevin Barnes <kbarnes3@gmail.com>
2023-12-08 11:27:09 -0800

    Day 06 Part 1 works with sample data

--- day06_wait_for_it_common/src/lib.rs
+++ day06_wait_for_it_common/src/lib.rs
@@ -1,1 +1,9 @@
-pub const SAMPLE_DATA: u64 = 288;
+pub struct RaceRecords<const N: usize> {
+    pub times: [u64; N],
+    pub distances: [u64; N],
+}
+
+pub const SAMPLE_DATA: RaceRecords<3> = RaceRecords {
+    times: [7, 15, 30],
+    distances: [9, 40, 200],
+};
--- day06_wait_for_it_part1/src/main.rs
+++ day06_wait_for_it_part1/src/main.rs
@@ -1,11 +1,43 @@
 #[allow(unused_imports)]
-use day06_wait_for_it_common::SAMPLE_DATA;
+use day06_wait_for_it_common::{RaceRecords, SAMPLE_DATA};
 
 fn main() {
     let result = do_work(SAMPLE_DATA);
     println!("{}", result);
 }
 
-fn do_work(data: u64) -> u64 {
-    data
+fn do_work<const N: usize>(data: RaceRecords<N>) -> u64 {
+    let mut result = 1;
+
+    for i in 0..N {
+        let time = data.times[i];
+        let distance = data.distances[i];
+        result *= count_winning_options(time, distance);
+    }
+
+    result
+}
+
+fn count_winning_options(time: u64, distance_to_beat: u64) -> u64 {
+    let mut result = 0;
+
+    for hold_time in 0..time {
+        let distance_for_hold_time = get_distance_for_hold_time(time, hold_time);
+        if distance_for_hold_time > distance_to_beat {
+            result += 1;
+        }
+    }
+
+    result
+}
+
+fn get_distance_for_hold_time(total_time: u64, hold_time: u64) -> u64 {
+    if hold_time > total_time {
+        panic!("hold_time must be less than or equal to total_time")
+    }
+
+    let running_time = total_time - hold_time;
+    let distance = running_time * hold_time;
+
+    distance
 }
