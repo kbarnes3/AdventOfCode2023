0038026fd3ac52394ff8762eadc6193eb57d2f5f
Kevin Barnes <kbarnes3@gmail.com>
2023-12-11 09:26:14 -0800

    WIP

--- day07_camel_cards_common/src/lib.rs
+++ day07_camel_cards_common/src/lib.rs
@@ -1,1 +1,16 @@
-pub const SAMPLE_DATA: u64 = 6440;
+pub struct HandBid {
+    pub hand: [char; 5],
+    pub bid: u64
+}
+
+// Transform with:
+// '<,'>s/\(.\)\(.\)\(.\)\(.\)\(.\) \(\d\+\)/{ HandBid { hand: [ '\1', '\2', '\3', '\4', '\5'], bid: \6 } },
+
+pub const SAMPLE_DATA: [HandBid; 5] = [
+    { HandBid { hand: [ '3', '2', 'T', '3', 'K'], bid: 765 } },
+    { HandBid { hand: [ 'T', '5', '5', 'J', '5'], bid: 684 } },
+    { HandBid { hand: [ 'K', 'K', '6', '7', '7'], bid: 28 } },
+    { HandBid { hand: [ 'K', 'T', 'J', 'J', 'T'], bid: 220 } },
+    { HandBid { hand: [ 'Q', 'Q', 'Q', 'J', 'A'], bid: 483 } },
+];
+
--- day07_camel_cards_part1/src/main.rs
+++ day07_camel_cards_part1/src/main.rs
@@ -1,11 +1,11 @@
 #[allow(unused_imports)]
-use day07_camel_cards_common::SAMPLE_DATA;
+use day07_camel_cards_common::{HandBid, SAMPLE_DATA};
 
 fn main() {
     let result = do_work(SAMPLE_DATA);
     println!("{}", result);
 }
 
-fn do_work(data: u64) -> u64 {
-    data
+fn do_work<const N: usize>(data: [HandBid; N]) -> u64 {
+    data[0].bid
 }
