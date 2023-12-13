1ea87fd4bf0d1c6ffd6bb7af4d0874514c7d9579
Kevin Barnes <kbarnes3@gmail.com>
2023-12-12 18:12:13 -0800

    Day 08 Part 1 works with sample data

--- day08_haunted_wasteland_common/src/lib.rs
+++ day08_haunted_wasteland_common/src/lib.rs
@@ -1,1 +1,36 @@
-pub const SAMPLE_DATA: u64 = 2;
+pub enum Direction {
+    Left,
+    Right,
+}
+
+pub struct NodeLink<'a> {
+    pub node: &'a str,
+    pub left: &'a str,
+    pub right: &'a str,
+}
+
+pub struct Map<'a, const D: usize, const N: usize> {
+    pub directions: [Direction; D],
+    pub nodes: [NodeLink<'a>; N],
+}
+
+pub const START_NODE: &str = "AAA";
+pub const END_NODE: &str = "ZZZ";
+
+// Transform with:
+// '<,'>s/L/Direction::Left, /g | '<,'>s/R/Direction::Right, /g
+// '<,'>s/\(\w\+\) = (\(\w\+\), \(\w\+\))/{ NodeLink { node: "\1", left: "\2", right: "\3" } },
+
+pub const SAMPLE_DATA: Map<2, 7> = Map {
+    directions: [Direction::Right, Direction::Left, ],
+
+    nodes: [
+    { NodeLink { node: "AAA", left: "BBB", right: "CCC" } },
+    { NodeLink { node: "BBB", left: "DDD", right: "EEE" } },
+    { NodeLink { node: "CCC", left: "ZZZ", right: "GGG" } },
+    { NodeLink { node: "DDD", left: "DDD", right: "DDD" } },
+    { NodeLink { node: "EEE", left: "EEE", right: "EEE" } },
+    { NodeLink { node: "GGG", left: "GGG", right: "GGG" } },
+    { NodeLink { node: "ZZZ", left: "ZZZ", right: "ZZZ" } },
+    ],
+};
